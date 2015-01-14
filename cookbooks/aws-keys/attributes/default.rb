5719ca164bb3a12eb1e6bf4ea4dffd0bec5a6c7c
Christopher Haupt <chaupt@mobirobo.com>
2015-01-14 08:07:46 -0800

    Add AWS credentials

--- cookbooks/aws-keys/attributes/default.rb
+++ cookbooks/aws-keys/attributes/default.rb
@@ -1,8 +1,8 @@
 default[:s3_region_hostname] = 's3-us-west-2'
 default[:s3_region] = 'us-west-2'
 default[:s3_key_pairs] = {
-  production: { key: "KEY",
-                secret: "SECRET" },
-  staging: {    key: "KEY",
-                secret: "SECRET" }
+  production: { key: "AKIAJDK54U4IO6VJAJFA",
+                secret: "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" },
+  staging: {    key: "AKIAIA2ZMJVCXTDQFZKA",
+                secret: "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" }
 }
