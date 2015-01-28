aa7d2822e9e61b22eb62e72a71f498547bb7e696
Christopher Haupt <chaupt@mobirobo.com>
2015-01-28 11:08:24 -0800

    Fix swapped aws keys

--- cookbooks/aws-keys/attributes/default.rb
+++ cookbooks/aws-keys/attributes/default.rb
@@ -1,8 +1,8 @@
 default[:s3_region_hostname] = 's3'
 default[:s3_region] = nil
 default[:s3_key_pairs] = {
-  production: { key: "AKIAJDK54U4IO6VJAJFA",
+  staging: { key: "AKIAJDK54U4IO6VJAJFA",
                 secret: "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" },
-  staging: {    key: "AKIAIA2ZMJVCXTDQFZKA",
+  production: {    key: "AKIAIA2ZMJVCXTDQFZKA",
                 secret: "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" }
 }
